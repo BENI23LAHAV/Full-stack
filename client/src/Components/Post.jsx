9c553e76e29c98fea73ba8416f7b3b1b2492c5c1
BENI23LAHAV <beni0548472300@gmail.com>
2024-09-26 14:12:03 +0300

    change the show comments button to get it from the server, add delet post button, add FetchDelete function

--- client/src/Components/Post.jsx
+++ client/src/Components/Post.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useRef } from "react";
+import Fetch, { FetchDelete } from "./fetch";
 
 const Post = (props) => {
   const [clicked, setClicked] = useState(false);
@@ -9,6 +10,11 @@ const Post = (props) => {
   const title = props.title;
   const body = props.body;
   const id = props.id;
+  /**comments zone */
+  const commentsUrl = `http://localhost:4000/2/comments?postId=${id}`;
+  const postUrl = `http://localhost:4000/2/posts/?postId=${id}`;
+  const [commentsArr, setComentsArr] = useState([]);
+  /** end of comments */
   const [coments, setComents] = useState([]);
   let coment = useRef("");
   //   console.log("id" + id);
@@ -22,27 +28,26 @@ const Post = (props) => {
         if (e.target.className === "title") {
           setClicked((prev) => !prev);
         }
-      }}
-    >
+      }}>
       <h2>Message: {id}</h2>
       <h3 className="title">Title: {title}</h3>
       {clicked && <p>{body}</p>}
       <div className="buttons">
         <button
-          disabled={coments.length === 0}
+          // disabled={coments.length === 0}
           onClick={() => {
+            Fetch(commentsUrl, setComentsArr);
+
             setComentsClicked((prev) => !prev);
             setAddComent((prev) => !prev);
-          }}
-        >
+          }}>
           Show coments
         </button>
         {addComent && (
           <button
             onClick={() => {
               setAddComentButon((prev) => !prev);
-            }}
-          >
+            }}>
             Add coment
           </button>
         )}
@@ -57,16 +62,31 @@ const Post = (props) => {
               // setAddComent((prev) => !prev);
               setAddComentButon((prev) => !prev);
               setClicked((prev) => !prev);
-            }}
-          >
+            }}>
             Submit
           </button>
         )}
+        {
+          <button
+            onClick={() => {
+              FetchDelete(postUrl, setComentsArr);
+            }}>
+            🗑️ post
+          </button>
+        }
       </div>
       <ul>
         {comentsClicked &&
-          coments.map((item, index) => {
-            return <li key={index}>{item}</li>;
+          commentsArr.map((item, index) => {
+            return (
+              <li key={index}>
+                <ul>
+                  <li style={{ fontSize: "10px" }}>name: {item.name} </li>
+                  <li style={{ fontSize: "10px" }}>email: {item.email} </li>
+                  <li style={{ fontSize: "20px" }}>comments: {item.body} </li>
+                </ul>
+              </li>
+            );
           })}
       </ul>
     </div>
