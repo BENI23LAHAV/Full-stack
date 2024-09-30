5d35fbdd7ed37f06fdfa0d9520042907e01f03b9
Zipory <ITZipory5@gmail.com>
2024-09-30 15:50:23 +0300

    JSON for all,  regeister and login

--- server/app.js
+++ server/app.js
@@ -1,8 +1,8 @@
-// import "./users-pass.json";
+const fs = require("fs");
 const express = require("express");
 const mysql = require("mysql2");
 const cors = require("cors");
-const bcrypt = require('bcrypt');
+const bcrypt = require("bcrypt");
 const app = express();
 
 app.use(
@@ -60,66 +60,6 @@ app.get("/", (req, res) => {
   console.log(59, "hi");
 });
 
-/*-------------- testing zone ------------------ */
-app.get("/select/:id", (req, res) => {
-  console.log(req.params.id);
-
-  const id = req.params.id;
-  if (selectColumn(id) !== "column not found") {
-    connection.query(`SELECT * FROM ${id}`, (err, results) => {
-      if (err) {
-        console.error("Error fetching data:", err);
-        res.status(500).send("Error fetching data from the database");
-      } else {
-        res.json(results);
-      }
-    });
-  } else {
-    res.status(404).send("Column not found");
-    console.log("Column not found");
-  }
-});
-app.get("/select/:id/:pos", (req, res) => {
-  console.log(req.params.id, req.params.pos);
-
-  const id = req.params.id;
-  const pos = req.params.pos;
-  if (selectColumn(id) !== "column not found" && pos >= 0) {
-    connection.query(`SELECT * FROM ${id}`, (err, results) => {
-      if (err) {
-        console.error("Error fetching data:", err);
-        res.status(500).send("Error fetching data from the database");
-      } else if (pos >= results.length) {
-        res.status(404).send("position not found");
-        console.log("position not found");
-      } else {
-        res.json(results[pos]);
-      }
-    });
-  } else {
-    res.status(404).send("Column not found");
-    console.log("Column not found");
-  }
-});
-
-// app.get("/:id/posts", (req, res) => {
-
-//   connection.query("SELECT id, title, body FROM posts WHERE user_id=?;", [req.params.id], (err, results) => {
-//   if (err) {
-//     console.error(103, "Error fetching data:", err);
-//     res.status(500).send("Error fetching data from the database");
-//   }
-//   else if(!results[0]) {
-//     console.error(108, "Error fetching data:");
-//     res.status(500).send("no database found.");
-//   }
-//   else{
-//     res.send(results);
-//   }
-// });
-//
-// });
-
 /*----------------------get the user info ---------------*/
 app.get("/:id", (req, res) => {
   let id = req.params.id;
@@ -137,7 +77,7 @@ app.get("/:id", (req, res) => {
         console.error(135, "Error fetching data: no user");
         res.status(500).send("no database found.");
       } else {
-        res.send(results[0]);
+        res.send(JSON.stringify(results[0]));
       }
     }
   );
@@ -153,9 +93,11 @@ app.get("/:id/photos", (req, res) => {
     (err, results) => {
       if (err) {
         console.error(154, "Error fetching data:", err);
-        res.status(500).send("Error fetching data from the database");
+        res
+          .status(500)
+          .send(JSON.stringify("Error fetching data from the database"));
       } else {
-        res.send(results);
+        res.send(JSON.stringify(results));
       }
     }
   );
@@ -175,9 +117,11 @@ app.get("/:id/comments", (req, res) => {
     (err, results) => {
       if (err) {
         console.error(176, "Error fetching data:", err);
-        res.status(500).send("Error fetching data from the database");
+        res
+          .status(500)
+          .send(JSON.stringify("Error fetching data from the database"));
       } else {
-        res.send(results);
+        res.send(JSON.stringify(results));
       }
     }
   );
@@ -193,7 +137,7 @@ app.get("/:id/:folder", (req, res) => {
         console.error(192, "Error fetching data:", err);
         res.status(500).send("Error fetching data from the database");
       } else {
-        res.send(results);
+        res.send(JSON.stringify(results));
       }
     }
   );
@@ -209,10 +153,12 @@ app.delete("/:id/todos/", (req, res) => {
     (err, results) => {
       if (err) {
         console.error(210, "Error fetching data:", err);
-        res.status(500).send("Error fetching data from the database");
+        res
+          .status(500)
+          .send(JSON.stringify("Error fetching data from the database"));
       } else {
         console.log(221, results);
-        res.send("The deletion is complete.");
+        res.send(JSON.stringify("The deletion is complete."));
       }
     }
   );
@@ -228,7 +174,9 @@ app.delete("/:id/posts", (req, res) => {
     (err, results) => {
       if (err) {
         console.error(239, "Error fetching data:", err);
-        res.status(500).send("Error fetching data from the database");
+        res
+          .status(500)
+          .send(JSON.stringify("Error fetching data from the database"));
       } else {
         console.log(232, results);
         connection.query(
@@ -238,10 +186,16 @@ app.delete("/:id/posts", (req, res) => {
           (err, results) => {
             if (err) {
               console.error(239, "Error fetching data:", err);
-              res.status(500).send("Error fetching data from the database");
+              res
+                .status(500)
+                .send(JSON.stringify("Error fetching data from the database"));
             } else {
               console.log(242, results);
-              res.send("The deletion is complete (from posts and comments).");
+              res.send(
+                JSON.stringify(
+                  "The deletion is complete (from posts and comments)."
+                )
+              );
             }
           }
         );
@@ -261,10 +215,12 @@ app.delete("/:id/comments", (req, res) => {
     (err, results) => {
       if (err) {
         console.error(261, "Error fetching data:", err);
-        res.status(500).send("Error fetching data from the database");
+        res
+          .status(500)
+          .send(JSON.stringify("Error fetching data from the database"));
       } else {
         console.log(264, results);
-        res.send("The deletion is complete.");
+        res.send(JSON.stringify("The deletion is complete."));
       }
     }
   );
@@ -279,10 +235,12 @@ app.put("/:id/todos", (req, res) => {
     (err, results) => {
       if (err) {
         console.error(260, "Error fetching data:", err);
-        res.status(500).send("Error fetching data from the database");
+        res
+          .status(500)
+          .send(JSON.stringify("Error fetching data from the database"));
       } else {
         console.log(results);
-        res.send("The update is complete.");
+        res.send(JSON.stringify("The update is complete."));
       }
     }
   );
@@ -297,10 +255,12 @@ app.put("/:id/posts", (req, res) => {
     (err, results) => {
       if (err) {
         console.error(278, "Error fetching data:", err);
-        res.status(500).send("Error fetching data from the database");
+        res
+          .status(500)
+          .send(JSON.stringify("Error fetching data from the database"));
       } else {
         console.log(results);
-        res.send("The update is complete.");
+        res.send(JSON.stringify("The update is complete."));
       }
     }
   );
@@ -316,10 +276,12 @@ app.put("/:id/comments", (req, res) => {
     (err, results) => {
       if (err) {
         console.error(297, "Error fetching data:", err);
-        res.status(500).send("Error fetching data from the database");
+        res
+          .status(500)
+          .send(JSON.stringify("Error fetching data from the database"));
       } else {
         console.log(results);
-        res.send("The update is complete.");
+        res.send(JSON.stringify("The update is complete."));
       }
     }
   );
@@ -335,10 +297,12 @@ app.post("/:id/todos", (req, res) => {
     (err, results) => {
       if (err) {
         console.error(316, "Error fetching data:", err);
-        res.status(500).send("Error fetching data from the database");
+        res
+          .status(500)
+          .send(JSON.stringify("Error fetching data from the database"));
       } else {
         console.log(results);
-        res.send("The addition is complete.");
+        res.send(JSON.stringify("The addition is complete."));
       }
     }
   );
@@ -354,10 +318,12 @@ app.post("/:id/posts", (req, res) => {
     (err, results) => {
       if (err) {
         console.error(335, "Error fetching data:", err);
-        res.status(500).send("Error fetching data from the database");
+        res
+          .status(500)
+          .send(JSON.stringify("Error fetching data from the database"));
       } else {
         console.log(results);
-        res.send("The addition is complete.");
+        res.send(JSON.stringify("The addition is complete."));
       }
     }
   );
@@ -374,43 +340,69 @@ app.post("/:id/comments", (req, res) => {
     (err, results) => {
       if (err) {
         console.error(355, "Error adding comment:", err);
-        res.status(500).send("Error adding comment to the database");
+        res
+          .status(500)
+          .send(JSON.stringify("Error adding comment to the database"));
       } else {
         console.log(results);
-        res.send("The comment has been added successfully.");
+        res.send(JSON.stringify("The comment has been added successfully."));
       }
     }
   );
 });
 
-// app.get("/users", (req, res) => {
-//   connection.query("SELECT * FROM users", (err, results) => {
-//     if (err) {
-//       console.error("Error fetching data:", err);
-//       res.status(500).send("Error fetching data from the database");
-//     } else {
-//       res.json(results);
-//     }
-//   });
-// });
+/*---------------------- create a new user --------------- */
+app.post("/register", (req, res) => {
+  try {
+    let { userName, password } = req.body;
+    const listOfUsers = JSON.parse(
+      fs.readFileSync("./users-pass.json", "utf8")
+    );
+    if (Object.keys(listOfUsers).includes(userName)) {
+      res.send(JSON.stringify("alredy have that username."));
+    } else {
+      createUser(password, userName, listOfUsers);
+      res.send(JSON.stringify("Added a new user."));
+    }
+  } catch (error) {
+    console.log(401, error);
+    res.send(JSON.stringify("no data."));
+  }
+});
+
+/*---------------------- verify password -------------------------- */
+app.post("/login", (req, res) => {
+  try {
+    let { userName, password } = req.body;
+    const listOfUsers = JSON.parse(
+      fs.readFileSync("./users-pass.json", "utf8")
+    );
+    if (Object.keys(listOfUsers).includes(userName)) {
+      checkPassword(password, listOfUsers[userName], res);
+    } else {
+      res.send(JSON.stringify("no user in that name."));
+    }
+  } catch (error) {
+    res.send(JSON.stringify("no data"));
+  }
+});
 
 /*-----------------set listener open on port 4000 ------------------ */
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
 });
+/*------------------end of the express -------------------------- */
 
-
-
+/*------------------------ function place ----------------------- */
 
 // Function to hash a password
 async function hashPassword(normalPassword) {
   const numOfRounds = 10;
   try {
     const hash = await bcrypt.hash(normalPassword, numOfRounds);
-    console.log(409,'Hashed password:', hash);
     return hash;
   } catch (error) {
-    console.error(412, 'Error hashing password:', error);
+    console.error(412, "Error hashing password:", error);
   }
 }
 
@@ -418,27 +410,21 @@ async function hashPassword(normalPassword) {
 async function verifyPassword(normalPassword, hashedPassword) {
   try {
     const isMatch = await bcrypt.compare(normalPassword, hashedPassword);
-    console.log(420, 'Password match:', isMatch);
     return isMatch;
   } catch (error) {
-    console.error(423, 'Error verifying password:', error);
+    console.error(423, "Error verifying password:", error);
   }
 }
 
-
-/* Example usage
-async function example() {
-  const password = 'mySecurePassword123';
-  
-  // Hash the password
-  const hashedPassword = await hashPassword(password);
-  
-  // Verify the password (should return true)
-  await verifyPassword(password, hashedPassword);
-  
-  // Verify with wrong password (should return false)
-  await verifyPassword('wrongPassword', hashedPassword);
+// Function to create a user with hash password
+async function createUser(password, userName, listOfUsers) {
+  let hash = await hashPassword(password);
+  listOfUsers[userName] = hash;
+  fs.writeFileSync("./users-pass.json", JSON.stringify(listOfUsers, null, 2));
 }
 
-example();      */
-
+// Function to verify a password from the post
+async function checkPassword(normalPassword, hashedPassword, res) {
+  let isMatch = await verifyPassword(normalPassword, hashedPassword);
+  isMatch ? res.send(JSON.stringify("good")) : res.send(JSON.stringify("bad"));
+}
