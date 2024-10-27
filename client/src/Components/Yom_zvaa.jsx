24f69ef46e4093b332065b1417472666a7041459
BENI23LAHAV <beni0548472300@gmail.com>
2024-10-27 13:01:01 +0200

    adding Yom zvaa font

--- client/src/Components/Yom_zvaa.jsx
+++ client/src/Components/Yom_zvaa.jsx
@@ -4,7 +4,7 @@ import React from "react";
 const Yom_zvaa = () => {
   return (
     <div>
-      <h1>יום זוועה, ביי</h1>
+      <h1 className="rubik-wet-paint-regular">יום זוועה, ביי</h1>
       {/* <img
         src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ4etDiWIf966TJHRnVFD-EdEz8kTNCIj2H0g&s"
         alt="ברקוני "
